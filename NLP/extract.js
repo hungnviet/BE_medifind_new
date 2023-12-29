8f46f57066bef37ba32f2fbe55e50a8cf8b790b8
Thu Nguyen <painkiller.nhat@gmail.com>
2023-12-29 18:14:50 +0700

    rewrite extract

---
+++ NLP/extract.js
@@ -0,0 +1,8 @@
+//import { PythonShell } from 'python-shell';
+process.env.PYTHONIOENCODING = 'utf-8';
+const { PythonShell } = require('python-shell');
+PythonShell.run('extract.py', null, function (err, result) {
+  if (err) throw err;
+  // result is an array consisting of messages collected during execution
+  console.log('result: ', result.toString());
+});
